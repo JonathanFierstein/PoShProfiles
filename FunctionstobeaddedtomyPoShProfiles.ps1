4e6242931c9f356a61c903520aeb06246ca79012
Jonathan Fierstein <48401556+JonathanFierstein@users.noreply.github.com>
2023-01-16 03:50:35 -0800

    Made some minor code updates to ConvertTo-Base64EncodedString Added ConvertFrom-Base64EncodedString

--- FunctionstobeaddedtomyPoShProfiles.ps1
+++ FunctionstobeaddedtomyPoShProfiles.ps1
@@ -12,9 +12,10 @@
 	
 	.EXAMPLE
 		PS C:\> ConvertTo-Base64EncodedString -InputString 'This string will be encoded.'
+		VABoAGkAcwAgAHMAdAByAGkAbgBnACAAdwBpAGwAbAAgAGIAZQAgAGUAbgBjAG8AZABlAGQALgA=
 	
 	.NOTES
-		Additional information about the function.
+		1-15-2023 todo:  Add ability for function to accept an array of strings as input.
 #>
 function ConvertTo-Base64EncodedString
 {
@@ -38,10 +39,10 @@ function ConvertTo-Base64EncodedString
 	Process
 	{
 		
-		Write-Verbose -Message "Converting $InputString to a [Byte] array."
+		Write-Verbose -Message "Converting the [string] `"$InputString`" to a [byte] array."
 		$ByteArray = [System.Text.Encoding]::Unicode.GetBytes($InputString)
 		
-		Write-Verbose -Message "Base64 Encoding the [Byte] array created from the input [string]."
+		Write-Verbose -Message "Base64 encoding the [byte] array created from the input [string]."
 		$EncodedOutputString = [Convert]::ToBase64String($ByteArray)
 		
 		
@@ -55,6 +56,66 @@ function ConvertTo-Base64EncodedString
 	}
 } #ConvertTo-Base64EncodedString
 
+
+
+
+<#
+	.SYNOPSIS
+		Converts a base64 encoded string to its cleartext counterpart.
+	
+	.DESCRIPTION
+		This function decodes a previously base64 encoded [string] back to cleartext.
+	
+	.PARAMETER EncodedString
+		This should be a base64 encoded [string].
+	
+	.EXAMPLE
+		$EncodedString = ConvertTo-Base64EncodedString -InputString "This is a string."
+		$EncodedString
+		VABoAGkAcwAgAGkAcwAgAGEAIABzAHQAcgBpAG4AZwAuAA==
+		
+		PS C:\> ConvertFrom-Base64EncodedString -EncodedString $EncodedString
+		"This is a string."
+	
+	.NOTES
+		1-15-2023 todo:  Add ability for function to accept an array of strings as input.
+#>
+function ConvertFrom-Base64EncodedString
+{
+	[CmdletBinding(ConfirmImpact = 'None',
+				   PositionalBinding = $true)]
+	[OutputType([string])]
+	param
+	(
+		[Parameter(Mandatory = $true,
+				   ValueFromPipeline = $true,
+				   ValueFromPipelineByPropertyName = $true,
+				   Position = 0,
+				   HelpMessage = 'a base64 encoded [string] to be decoded')]
+		[AllowEmptyString()]
+		[ValidateNotNull()]
+		[string]$EncodedString
+	)
+	
+	Begin
+	{
+		Write-Verbose -Message "Starting $($MyInvocation.MyCommand)"
+	}
+	Process
+	{
+		Write-Verbose -Message 'Decoding the input string and storing it in a [byte] array.'
+		$Bytes = [System.Convert]::FromBase64String($EncodedString)
+		
+		Write-Verbose -Message 'Converting the decoded [byte] array into Unicode cleartext.'
+		$DecodedString = [System.Text.Encoding]::Unicode.GetString($Bytes)
+		
+		Write-Output $DecodedString
+	}
+	End
+	{
+		Write-Verbose -Message "Ending $($MyInvocation.MyCommand)"
+	}
+} #ConvertFrom-Base64EncodedString
 
 
 
